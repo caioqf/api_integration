da22f1aec965cc9ab91989e92341b71662674dbe
caioqf <caio8200@gmail.com>
2022-10-22 00:05:40 -0300

    add: routes before serverless

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import 'dotenv/config'
 import express from 'express'
 import getSheet from './sheet/getSheet.js'
 import saveContact from './contact/saveContact.js'
+import getContact from './contact/getContacts.js'
+import bodyParser from 'body-parser'
 
 const API_PORT = process.env.API_PORT
+
 const app = express();
 
-app.get("/getSheet", getSheet)
+app.use(bodyParser.json())
+
+
+app.get("/sheet", getSheet)
 app.post("/contact", saveContact)
+app.get("/contact", getContact)
 
 
 app.listen(API_PORT, () => {
